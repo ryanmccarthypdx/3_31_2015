67c8ed6617a5600ddea11567d08d533e5e0728b9
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-23 10:50:45 -0700

    Add user model functionality including bcrypt; all green tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   has_many :questions
+
+  attr_accessor :password
+  validates_confirmation_of :password
+  before_save :encrypt_password
+  validates_uniqueness_of :name
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(name, password)
+    user = User.where(name: name).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
 end
