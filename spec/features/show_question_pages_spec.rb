a21e6a0988253e8c58f33e3fe92bfbb77d61303f
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-24 14:19:04 -0700

    Add conditionally appearing edit/delete buttons onto question

--- spec/features/show_question_pages_spec.rb
+++ spec/features/show_question_pages_spec.rb
@@ -1,15 +1,30 @@
 require "rails_helper"
 describe "show a question process" do
   let(:user) { FactoryGirl.create(:user) }
-  let!(:question) { FactoryGirl.create(:question) }
 
   before do
     sign_in(user)
   end
 
   it "will show a the title and body of a question" do
+    question = FactoryGirl.create(:question)
     visit questions_path
     click_on question.title
     expect(page).to have_content("So many yaks")
   end
+
+  it "will show the edit/delete links if the user is the creator of that question" do
+    question = FactoryGirl.create(:question, :user_id => user.id)
+    visit question_path(question)
+    expect(page).to have_content("Edit")
+  end
+
+  it "will not show the edit/delete links if the user isn't the creator of that question" do
+    question = FactoryGirl.create(:question)
+    visit questions_path
+    click_on question.title
+    expect(page).to have_no_content("Edit")
+  end
+
+
 end
