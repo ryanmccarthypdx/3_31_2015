303210f17a086386dba2d2c67c7fde06b463799b
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-24 14:50:37 -0700

    Add edit & delete functionality to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,31 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:notice] = "Successfully Updated!"
+      redirect_to questions_path
+    else
+      render:edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy
+      flash[:notice] = "Question Removed"
+      redirect_to questions_path
+    else
+      flash[:alert] = "Horrible failure"
+    redirect_to question_path(@question)
+    end
+  end
+
   def new
     @question = Question.new
   end
---
+++ spec/features/modify_question_pages_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+describe "edit/delete a question process" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    sign_in(user)
+  end
+
+  it "will open the edit form" do
+    question = FactoryGirl.create(:question, :user_id => user.id)
+    visit question_path(question)
+    click_on "Edit"
+    expect(page).to have_content("Title")
+  end
+
+  it "will edit the text of a question" do
+    question = FactoryGirl.create(:question, :user_id => user.id)
+    visit question_path(question)
+    click_on "Edit"
+    fill_in "Body", :with => "Lee kisses yaks on the mouth"
+    click_on "Ask question"
+    visit question_path(question)
+    expect(page).to have_content("Lee kisses yaks on the mouth")
+  end
+
+  it "will edit the title of a question" do
+    question = FactoryGirl.create(:question, :user_id => user.id)
+    visit question_path(question)
+    click_on "Edit"
+    fill_in "Title", :with => "How many yaks does Lee kiss?"
+    click_on "Ask question"
+    expect(page).to have_content("How many yaks does Lee kiss?")
+  end
+
+  it "will delete a question" do
+    question = FactoryGirl.create(:question, :user_id => user.id)
+    visit question_path(question)
+    click_on "Delete"
+    expect(page).to have_no_content("light bulb")
+  end
+end
